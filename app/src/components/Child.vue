26119a8d1478dad8b98347019989347eb32cd7c3
pham tuan <phamtuanchip@gmail.com>
2018-04-13 17:53:26 +0700

    implement inser or update db

--- app/src/components/Child.vue
+++ app/src/components/Child.vue
@@ -1,19 +1,53 @@
 <template>
   <div>
-  <b-card :title="child.Name"
+  <b-card v-if="isEdit == false" :title="child.Name"
           :img-src="child.Avatar"
           img-alt="Image"
           img-top
           tag="article"
           style="max-width: 20rem;"
           class="mb-2">
-    <span >{{child.Year}}</span>  
-    <span >{{child.DOB}}</span>     
+    <span >Sinh năm:{{child.Year}}</span>  
+    <span >Sinh nhật:{{child.DOB}}</span>     
     <p class="card-text">
       {{child.Description}}
     </p>
-    <b-button href="#" variant="primary">Chỉnh sửa</b-button>
+    <b-button @click="edit" variant="primary">Chỉnh sửa</b-button>
   </b-card>
+  <div v-else>
+
+    <f7-block-title>{{child.Name}}</f7-block-title>
+    <f7-list form>
+       <f7-list-item>
+        <f7-label>Id</f7-label>
+        <f7-input v-model="item.Id" type="text" placeholder="Name"></f7-input>
+      </f7-list-item>
+      <f7-list-item>
+        <f7-label>Name</f7-label>
+        <f7-input v-model="item.Name" type="text" placeholder="Name"></f7-input>
+      </f7-list-item>
+      <f7-list-item>
+        <f7-label >Gender</f7-label>
+        <f7-input v-model="item.Gender" type="select">
+          <option value="0">Male</option>
+          <option value="1">Female</option>
+        </f7-input>
+      </f7-list-item>
+      <f7-list-item>
+        <f7-label>Birth date</f7-label>
+        <f7-input  v-model="item.DOB" type="date" placeholder="Birth date" value="2014-04-30"></f7-input>
+      </f7-list-item>
+      <f7-list-item>
+        <f7-label>Avatar</f7-label>
+        <f7-input v-model="item.Avatar" type="url" placeholder="URL"></f7-input>
+      </f7-list-item>
+      <f7-list-item>
+        <f7-label>Description</f7-label>
+        <f7-input v-model="item.Description" type="textarea" placeholder="Textarea"></f7-input>
+      </f7-list-item>
+      <f7-col><f7-button big fill color="green" @click="save" >Save</f7-button></f7-col>
+    </f7-list>
+  </div>
 </div>
 </template>
 
@@ -22,36 +56,8 @@ export default {
   props: ["child"],
   data: function () {
     return {
-      rightDrawer: false,
-      right: true,
-      search: '',
-      errText: '',
-      pagination: {},
-      snackbar: false,
-      headers: [
-        {
-          text: 'First Name',
-          left: true,
-          sortable: false,
-          value: 'firstName'
-        },
-        { text: 'Last Name', value: 'lastName' },
-        { text: 'Email', value: 'email' },
-        { text: 'Age', value: 'age' },
-        { text: 'Order Record', value: 'orderRecord' },
-        { text: 'Active', value: 'isActive' },
-        { text: '', value: '' }
-      ],
-      items: [],
-      searchVm: {
-        contains: {
-          firstName: '',
-          lastName: ''
-        },
-        between: {
-          age: {former: 0, latter: 0}
-        }
-      }
+      isEdit: false,
+      item: this.child
     }
   },
   
@@ -59,6 +65,39 @@ export default {
   },
   mounted: function () {
      
+  },
+  methods: {
+    edit(){
+      this.isEdit = true;
+    },
+    
+    save: function () {
+      let customer = this.item
+      if (!customer.Id) {
+        this.api.postData('children', customer).then((res) => {           
+          this.isEdit = false;
+        }, (err) => {
+          console.log(err)
+        })
+      } else {
+        this.api.putData('children/' + customer.Id.toString(), customer).then((res) => {           
+           this.isEdit = false;
+        }, (err) => {
+          console.log(err)
+        })
+      }
+    },
+    getById: function () {
+      this.api.getData('children/' + customer.Id).then((res) => {
+        this.item = res.data
+        // this.customer.avatar = '/assets/' + this.customer.avatar
+      }, (err) => {
+        console.log(err)
+      })
+    },
+    cancel: function () {
+       
+    }
   }
 }
 </script>

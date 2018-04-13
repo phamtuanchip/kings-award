bb5c01720a9c90c3a2cf4e417970d51d966756ac
pham tuan <phamtuanchip@gmail.com>
2018-04-13 16:25:11 +0700

    add db and new timeline template

--- app/src/components/TimeLine.vue
+++ app/src/components/TimeLine.vue
@@ -1,100 +1,81 @@
 <template>
-  <div class="container">
-    <div class="page-header">
-        <h1 id="timeline">Timeline</h1>
+   <div class="timeline">
+  <!-- Timeline item -->
+  <div class="timeline-item">
+    <div class="timeline-item-date">21 <small>DEC</small></div>
+    <div class="timeline-item-divider"></div>
+    <div class="timeline-item-content">Plain text item</div>
+  </div>
+  <!-- Timeline item with inner -->
+  <div class="timeline-item">
+    <div class="timeline-item-date">22 <small>DEC</small></div>
+    <div class="timeline-item-divider"></div>
+    <div class="timeline-item-content">
+      <div class="timeline-item-inner">Another text goes here</div>
     </div>
-    <ul class="timeline">
-        <li>
-          <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-              <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
-            </div>
-            <div class="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-            </div>
-          </div>
-        </li>
-        <li class="timeline-inverted">
-          <div class="timeline-badge warning"><i class="glyphicon glyphicon-credit-card"></i></div>
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-            </div>
-            <div class="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-              <p>Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa.</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="timeline-badge danger"><i class="glyphicon glyphicon-credit-card"></i></div>
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-            </div>
-            <div class="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-            </div>
-          </div>
-        </li>
-        <li class="timeline-inverted">
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-            </div>
-            <div class="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="timeline-badge info"><i class="glyphicon glyphicon-floppy-disk"></i></div>
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-            </div>
-            <div class="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-              <hr>
-              <div class="btn-group">
-                <button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown">
-                  <i class="glyphicon glyphicon-cog"></i> <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu" role="menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li class="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-            </div>
-            <div class="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-            </div>
-          </div>
-        </li>
-        <li class="timeline-inverted">
-          <div class="timeline-badge success"><i class="glyphicon glyphicon-thumbs-up"></i></div>
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-            </div>
-            <div class="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-            </div>
-          </div>
-        </li>
-    </ul>
+  </div>
+  <!-- Timeline item with special timeline elements -->
+  <div class="timeline-item">
+    <div class="timeline-item-date">23 <small>DEC</small></div>
+    <div class="timeline-item-divider"></div>
+    <div class="timeline-item-content">
+      <div class="timeline-item-time">12:33</div>
+      <div class="timeline-item-title">Item Title</div>
+      <div class="timeline-item-subtitle">Item Subtitle</div>
+      <div class="timeline-item-text">Item Text</div>
+    </div>
+  </div>
+  <!-- Timeline item with special timeline elements and inner -->
+  <div class="timeline-item">
+    <div class="timeline-item-date">24 <small>DEC</small></div>
+    <div class="timeline-item-divider"></div>
+    <div class="timeline-item-content">
+      <div class="timeline-item-inner">
+        <div class="timeline-item-time">12:33</div>
+        ...
+      </div>
+    </div>
+  </div>
+  <!-- Timeline item with multiple events per day  -->
+  <div class="timeline-item">
+    <div class="timeline-item-date">25 <small>DEC</small></div>
+    <div class="timeline-item-divider"></div>
+    <div class="timeline-item-content">
+      <div class="timeline-item-time">12:33</div>
+      <div class="timeline-item-title">Item Title</div>
+      <div class="timeline-item-subtitle">Item Subtitle</div>
+      <div class="timeline-item-text">Item Text</div>
+      <div class="timeline-item-time">15:45</div>
+      <div class="timeline-item-title">Item Title</div>
+      <div class="timeline-item-subtitle">Item Subtitle</div>
+      <div class="timeline-item-text">Item Text</div>
+    </div>
+  </div>
+  <!-- Timeline item with multiple events (inners) per day -->
+  <div class="timeline-item">
+    <div class="timeline-item-date">26 <small>DEC</small></div>
+    <div class="timeline-item-divider"></div>
+    <div class="timeline-item-content">
+      <div class="timeline-item-inner">
+        <div class="timeline-item-time">12:33</div>
+        ...
+      </div>
+      <div class="timeline-item-inner">
+        <div class="timeline-item-time">15:45</div>
+        ...
+      </div>
+    </div>
+  </div>
+  <!-- Timeline item with Card -->
+  <div class="timeline-item">
+    <div class="timeline-item-date">27 <small>DEC</small></div>
+    <div class="timeline-item-divider"></div>
+    <div class="timeline-item-content card">
+      <div class="card-header">Card header</div>
+      <div class="card-content card-content-padding">Card content</div>
+      <div class="card-footer">Card footer</div>
+    </div>
+  </div>
 </div>
 </template>
 <script>
